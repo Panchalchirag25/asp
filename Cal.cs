03df1c561a86444f25e489ee793503d18556ecdc
Panchal Chirag <111890961+Panchalchirag25@users.noreply.github.com>
2022-12-05 21:11:38 +0530

    Add files via upload

---
+++ Cal.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculator_66
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        float num, ans;
+        int count;
+        private void button8_Click(object sender, EventArgs e)
+        {
+            txtAns.Text += 7;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            txtAns.Text += 9;
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            txtAns.Text += 1;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            txtAns.Text += 2;
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txtAns.Text += 3;
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            txtAns.Text += 4;
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            txtAns.Text += 5;
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            txtAns.Text += 6;
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            txtAns.Text += 8;
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            txtAns.Text += 0;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            txtAns.Text = txtAns.Text + ".";
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            int length = txtAns.TextLength-1;
+            string text = txtAns.Text;
+            txtAns.Clear();
+            for (int i = 0; i < length; i++)
+            {
+                txtAns.Text = txtAns.Text + text[i];
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtAns.Text = "";
+        }
+
+        private void btnequal_Click(object sender, EventArgs e)
+        {
+            compute();
+            label1.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            num = float.Parse(txtAns.Text);
+            txtAns.Clear();
+            
+            txtAns.Focus();
+            count = 1;
+            label1.Text = num.ToString() + "+";
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            num = float.Parse(txtAns.Text);
+            txtAns.Clear();
+            txtAns.Focus();
+            count = 2;
+            label1.Text = num.ToString() + "‐";
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            num = float.Parse(txtAns.Text);
+            txtAns.Clear();
+            txtAns.Focus();
+            count = 3;
+            label1.Text = num.ToString() + "*";
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            num = float.Parse(txtAns.Text);
+            txtAns.Clear();
+            txtAns.Focus();
+            count = 4;
+            label1.Text = num.ToString() + "/";
+        }
+
+        public void compute()
+        {
+            switch (count)
+            {
+                case 1:
+                    ans = num + float.Parse(txtAns.Text);
+                    txtAns.Text = ans.ToString();
+                    break;
+                case 2:
+                    ans = num - float.Parse(txtAns.Text);
+                    txtAns.Text = ans.ToString();
+                    break;
+                case 3:
+                    ans = num * float.Parse(txtAns.Text);
+                    txtAns.Text = ans.ToString();
+                    break;
+                case 4:
+                    ans = num / float.Parse(txtAns.Text);
+                    txtAns.Text = ans.ToString();
+                    break;
+                    
+            default:
+                    break;
+            }
+        }
+    }
+}
+
---
+++ Tic.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TIC_TAC_TOE
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true;
+        int turn_count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutGToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tic Tac Toe About");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "x";
+            else
+                b.Text = "0";
+
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+            checkforwinner();
+        }
+
+        private void checkforwinner()
+        {
+            bool winner = false;
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text)&&(!A1.Enabled))
+            {
+                winner = true;
+            }
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+            {
+                winner = true;
+            }
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+            {
+                winner = true;
+            }
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+            {
+                winner = true;
+            }
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!C1.Enabled))
+            {
+                winner = true;
+            }
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+            {
+                winner = true;
+            }
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+            {
+                winner = true;
+            }
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+            {
+                winner = true;
+            }
+            if (winner)
+            {
+                disableButton();
+                String winnerer = "";
+                if (turn)
+                    winnerer = "0";
+                else
+                    winnerer = "x";
+                Console.Beep();
+                MessageBox.Show(winnerer+"wins..!","Yah...!");
+                
+            }
+            else{
+                if(turn_count==9)
+                    MessageBox.Show("Draw...!","Bummer");
+            }
+        }
+
+        private void disableButton()
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+            }
+            catch { }
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            turn = true;
+            turn_count = 0;
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                }
+            }
+            catch {
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
